055491a5e9db4d6691dd42ca76faf978e99b59a5
huang <1315970725@qq.com>
2019-05-12 23:47:34 +0800

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Handlers;
+use Image;
 
 class ImageUploadHandler
 {
@@ -7,7 +8,7 @@ class ImageUploadHandler
     protected $allowed_ext = ["png", "jpg", "gif", "jpeg"];
 
     //file:文件对象  folder：目录 file_prefix：文件前缀
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, $max_width= false)
     {
         //文件夹切割让查找效率更高
         $folder_name = "uploads/images/$folder/".date("Ym/d",time());
@@ -29,8 +30,34 @@ class ImageUploadHandler
 
         //将图片移动到我们的目标存储路径中
         $file->move($upload_path,$filename);
+
+        //如果限制了图片宽度,就进行剪裁
+        if($max_width && $extension !='gif')
+        {
+            //此类中封装的函数,用于裁剪图片
+            $this->reduceSize($upload_path.'/'.$filename,$max_width);
+        }
+
         return [
             'path'=>config('app.url')."/$folder_name/$filename"
         ];
+    }
+
+    public function reduceSize($file_path,$max_width)
+    {
+       //先实例化,传参是文件的磁盘物理路径
+       $image = Image::make($file_path,$max_width);
+
+       //进行大小的调整
+       $image->resize($max_width,null,function($constraint){
+          //设定宽度是 $max_width,高度等比例缩放
+          $constraint->aspectRatio();
+
+          //防止裁图时尺寸变大
+          $constraint->upsize();
+       });
+
+       //对图片修改后进行保存
+       $image->save();
     }
 }
