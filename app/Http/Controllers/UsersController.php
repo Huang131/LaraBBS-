7283217deab779348d81fec2b4e79ceaf4272fd2
huang <1315970725@qq.com>
2019-05-13 13:48:10 +0800

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['show']]);
+    }
+
     public function show(User $user)
     {
 
@@ -17,12 +22,15 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader,User $user)
     {
+        $this->authorize('update',$user);
         $data = $request->all();
+
         if($request->avatar)
         {
             $result = $uploader->save($request->avatar,'avatars',$user->id,416);
@@ -31,6 +39,7 @@ class UsersController extends Controller
                 $data['avatar'] = $result['path'];
             }
         }
+
         $user->update($data);
         return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功!');
     }
