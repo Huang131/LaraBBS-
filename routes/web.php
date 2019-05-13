e20be31a2d4451abdc3ff079248f60ccbb3f763d
huang <1315970725@qq.com>
2019-05-13 18:49:32 +0800

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 
 // Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 
-// Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 // Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
